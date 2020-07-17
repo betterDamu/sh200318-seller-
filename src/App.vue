1f84e0f52d0edecbc804095096fcfce3d9dbffdf
zdylg <zdylg@1314.com>
2020-07-17 14:42:47 +0800

    完成导航

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <seller-header></seller-header>
     <div class="navs">
       <div class="nav">
-        <router-link to="/goods">goods</router-link>
+        <router-link to="/goods">商品</router-link>
       </div>
       <div class="nav">
-        <router-link to="/ratings">ratings</router-link>
+        <router-link to="/ratings">评价</router-link>
       </div>
       <div class="nav">
-        <router-link to="/seller">seller</router-link>
+        <router-link to="/seller">商家</router-link>
       </div>
     </div>
     <router-view></router-view>
@@ -35,9 +35,23 @@
 </script>
 
 <style scoped lang="stylus">
+  @import "./common/stylus/mixin.styl"
   #app
     .navs
+      one-px(black)
+      display flex
+      height 40px
       .nav
+        flex 1
         a
-          color red
+          display flex
+          justify-content center
+          align-items center
+          width 100%
+          height 100%
+          font-size 14px
+          color rgba(77,85,93,1)
+          &.active
+            color rgba(240,20,20,1)
+
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,8 @@ import routes from "routes/routes.js"
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    routes
+    routes,
+    linkActiveClass:"active",
+    linkExactActiveClass:""
 })
 
