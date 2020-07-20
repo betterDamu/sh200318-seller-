be8f540d2cde9ab4ae8727c2901a3743597e9bf3
zdylg <zdylg@1314.com>
2020-07-20 16:13:24 +0800

    完成右侧同步左侧

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -47,49 +47,70 @@
                 //根据右侧的滑屏情况来决定当前应该返回怎样的下标
                 //需要当前右侧列表滑动的实时距离 : scrollY
                 //需要所有右侧分类项高度组成的一个累加数组 : heightArr
-                return 0
+
+                var index = 0;
+
+                index = this.heightArr.findIndex((item,index,arr)=>{
+                    return (this.scrollY >= item && this.scrollY < arr[index+1])
+                })
+
+
+                return index
             }
         },
         methods:{
-            ...mapActions([GETSGOODS])
+            ...mapActions([GETSGOODS]),
+            //初始化滑屏
+            initScroll(){
+                this.$nextTick(()=>{
+                    //将滑屏的包裹器传入到BScroll内部就可以产生滑屏
+                    this.leftScroll = new BScroll(this.$refs.left);
+                    //计算得到右侧滑屏元素移动的实时距离(正值)
+                    this.rightScroll =new BScroll(this.$refs.right,{
+                        probeType:3
+                    });
+                    this.rightScroll.on("scroll",({x, y})=>{
+                        this.scrollY = Math.abs(y)
+                    })
+                })
+            },
+            //初始化heightArr
+            initHeightArr(){
+                /*1. 有没有可能在当前代码被执行时  仓库中goods的数据还是空的?
+                    当前代码在执行时 我们请求goods这个接口还没有响应
+                    响应式更新还没起作用 这个时候去操作一个dom节点的相关api是肯定不安全的
+                2. 有没有可能在当前代码被执行时 仓库中goods的数据已经被填上了?
+                        哪怕当前代码在被执行时 数据已经回来了 可是mounted阶段
+                        只是来做挂载的 它并不能保证界面已经完成渲染
+                        这个时候去操作一个dom节点的相关api是有可能不安全的*/
+
+                /*vm.$nextTick(cb)
+                    将cb延迟到下次 DOM 更新循环之后执行。
+                    在修改数据之后立即使用它，然后等待 DOM 更新
+                    在cb中的代码执行 上一次数据引起的dom更新百分百已经完成渲染了!!!
+                */
+                //确保当前的这个注册 在goods数据被修改之后再进行
+                this.$nextTick(()=>{
+                    //计算得到所有分类列表高度累加的一个数组
+                    //children会剔除所有的文本节点
+                    //childNodes会包含文本节点
+                    let rightLiNodes = this.$refs.classList.children;
+                    let heights = [];
+                    let res = Array.from(rightLiNodes).reduce((adder,item)=>{
+                        //offsetHeight : 代表的是元素的高度
+                        heights.push(adder);
+                        return adder + item.offsetHeight
+                    },0)
+                    heights.push(res);
+                    this.heightArr = heights
+                })
+            }
         },
         //mounted代表挂载完成 但是挂载完成并不代表页面渲染成功
         async mounted(){
             await this[GETSGOODS]();
-            //将滑屏的包裹器传入到BScroll内部就可以产生滑屏
-            this.leftScroll = new BScroll(this.$refs.left);
-            //计算得到右侧滑屏元素移动的实时距离(正值)
-            this.rightScroll =new BScroll(this.$refs.right,{
-                probeType:3
-            });
-            this.rightScroll.on("scroll",({x, y})=>{
-                this.scrollY = Math.abs(y)
-            })
-
-            //计算得到所有分类列表高度累加的一个数组
-            //children会剔除所有的文本节点
-            //childNodes会包含文本节点
-
-            /*1. 有没有可能在当前代码被执行时  仓库中goods的数据还是空的?
-                    当前代码在执行时 我们请求goods这个接口还没有响应
-                    响应式更新还没起作用 这个时候去操作一个dom节点的相关api是肯定不安全的
-            2. 有没有可能在当前代码被执行时 仓库中goods的数据已经被填上了?
-                    哪怕当前代码在被执行时 数据已经回来了 可是mounted阶段
-                    只是来做挂载的 它并不能保证界面已经完成渲染
-                    这个时候去操作一个dom节点的相关api是有可能不安全的*/
-
-            /*vm.$nextTick(cb)
-                将cb延迟到下次 DOM 更新循环之后执行。
-                在修改数据之后立即使用它，然后等待 DOM 更新
-                在cb中的代码执行 上一次数据引起的dom更新百分百已经完成渲染了!!!
-            */
-
-            //确保当前的这个注册 在goods数据被修改之后再进行
-            this.$nextTick(()=>{
-                let rightLiNodes = this.$refs.classList.children;
-                console.log(rightLiNodes.length);
-            })
-
+            this.initScroll();
+            this.initHeightArr();
         },
         components:{
             "seller-food":food
